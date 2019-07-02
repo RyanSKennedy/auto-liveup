861ada368ebf534ecb8b6ed564fd8015b3ab41f4
Unknown <ryan_s.kennedy@mail.ru>
2019-07-02 12:58:55 +0300

    Add logics for settings time and keys combination for refresh session

--- AutoLiveUp/AutoLiveUp/MainWindow.xaml.cs
+++ AutoLiveUp/AutoLiveUp/MainWindow.xaml.cs
@@ -20,6 +20,12 @@ namespace AutoLiveUp
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region
+        public string refreshControl;
+        public int refreshTime = 0;
+        #endregion
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -37,12 +43,82 @@ namespace AutoLiveUp
 
         private void ComboBox_CombinationKey_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            switch (ComboBox_CombinationKey.SelectedValue.ToString())
+            {
+                case "Ctrl (Default)":
+                    refreshControl = "{^}";
+                    break;
 
+                case "Backspace":
+                    refreshControl = "{BACKSPACE}";
+                    break;
+
+                case "Shift":
+                    refreshControl = "{+}";
+                    break;
+
+                case "Numlock":
+                    refreshControl = "{NUMLOCK}";
+                    break;
+                     
+                case "Capslock":
+                    refreshControl = "{CAPSLOCK}";
+                    break;
+
+                case "Alt+Tab":
+                    refreshControl = "{% TAB}";
+                    break;
+
+                default:
+                    refreshControl = "{^}";
+                    break;
+            }
         }
 
         private void ComboBox_RefreshTimeout_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            switch (ComboBox_RefreshTimeout.SelectedValue.ToString())
+            {
+                case "30 sec":
+                    refreshTime = 30000;
+                    break;
 
+                case "1 min":
+                    refreshTime = 60000;
+                    break;
+
+                case "3 min (Default)":
+                    refreshTime = 180000;
+                    break;
+
+                case "5 min":
+                    refreshTime = 300000;
+                    break;
+
+                case "10 min":
+                    refreshTime = 600000;
+                    break;
+
+                case "15 min":
+                    refreshTime = 900000;
+                    break;
+
+                case "20 min":
+                    refreshTime = 1200000;
+                    break;
+
+                case "30 min":
+                    refreshTime = 1800000;
+                    break;
+
+                case "1 hour":
+                    refreshTime = 3600000;
+                    break;
+
+                default:
+                    refreshTime = 180000;
+                    break;
+            }
         }
     }
 }
