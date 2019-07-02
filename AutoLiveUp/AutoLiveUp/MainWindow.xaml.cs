744ae6fc465a74ef0f1d4c7dafe202b2c1104fa3
Unknown <ryan_s.kennedy@mail.ru>
2019-07-03 01:28:39 +0300

    Add mechanism hook from keyboard and background timer for check session

--- AutoLiveUp/AutoLiveUp/MainWindow.xaml.cs
+++ AutoLiveUp/AutoLiveUp/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
+using Utilities;
 
 namespace AutoLiveUp
 {
@@ -20,25 +12,56 @@ namespace AutoLiveUp
     /// </summary>
     public partial class MainWindow : Window
     {
-        #region
-        public string refreshControl;
-        public int refreshTime = 0;
+        #region Variable declaration
+        globalKeyboardHook gkh = new globalKeyboardHook();
+        public DispatcherTimer dispatcherTimer;
+        public Key refreshControl = Key.LeftCtrl;
+        public int refreshTime = 180000;
+        public static int i = 0;
         #endregion
 
 
         public MainWindow()
         {
             InitializeComponent();
+            
+            //  DispatcherTimer setup
+            // Timer using for background checking activity
+            dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 1);
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            i++;
+
+            if (i*1000 >= refreshTime)
+            {
+                Send(refreshControl);
+                i = 0;
+            }
+
+            // Forcing the CommandManager to raise the RequerySuggested event
+            CommandManager.InvalidateRequerySuggested();
         }
 
         private void Button_Stop_Click(object sender, RoutedEventArgs e)
         {
-
+            //unhook
+            gkh.unhook();
+            dispatcherTimer.Stop();
+            Button_Stop.IsEnabled = false;
+            Button_Start.IsEnabled = true;
         }
 
         private void Button_Start_Click(object sender, RoutedEventArgs e)
         {
-
+            //start the hook 
+            gkh.hook();
+            dispatcherTimer.Start();
+            Button_Stop.IsEnabled = true;
+            Button_Start.IsEnabled = false;
         }
 
         private void ComboBox_CombinationKey_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -46,31 +69,27 @@ namespace AutoLiveUp
             switch (ComboBox_CombinationKey.SelectedValue.ToString())
             {
                 case "Ctrl (Default)":
-                    refreshControl = "{^}";
+                    refreshControl = Key.LeftCtrl;
                     break;
 
-                case "Backspace":
-                    refreshControl = "{BACKSPACE}";
+                case "Space":
+                    refreshControl = Key.Space;
                     break;
 
                 case "Shift":
-                    refreshControl = "{+}";
+                    refreshControl = Key.LeftShift;
                     break;
 
                 case "Numlock":
-                    refreshControl = "{NUMLOCK}";
+                    refreshControl = Key.NumLock;
                     break;
                      
                 case "Capslock":
-                    refreshControl = "{CAPSLOCK}";
-                    break;
-
-                case "Alt+Tab":
-                    refreshControl = "{% TAB}";
+                    refreshControl = Key.CapsLock;
                     break;
 
                 default:
-                    refreshControl = "{^}";
+                    refreshControl = Key.LeftCtrl;
                     break;
             }
         }
@@ -118,6 +137,50 @@ namespace AutoLiveUp
                 default:
                     refreshTime = 180000;
                     break;
+            }
+        }
+
+        /// <summary>
+        ///   Sends the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        public static void Send(Key key)
+        {
+            if (Keyboard.PrimaryDevice != null)
+            {
+                if (Keyboard.PrimaryDevice.ActiveSource != null)
+                {
+                    if (key == Key.NumLock || key == Key.CapsLock)
+                    {
+                        var e = new KeyEventArgs(Keyboard.PrimaryDevice, Keyboard.PrimaryDevice.ActiveSource, 0, key)
+                        {
+                            RoutedEvent = Keyboard.KeyDownEvent
+                        };
+
+                        InputManager.Current.ProcessInput(e);
+
+                        e = new KeyEventArgs(Keyboard.PrimaryDevice, Keyboard.PrimaryDevice.ActiveSource, 0, key)
+                        {
+                            RoutedEvent = Keyboard.KeyDownEvent
+                        };
+
+                        InputManager.Current.ProcessInput(e);
+                    }
+                    else
+                    {
+                        var e = new KeyEventArgs(Keyboard.PrimaryDevice, Keyboard.PrimaryDevice.ActiveSource, 0, key)
+                        {
+                            RoutedEvent = Keyboard.KeyDownEvent
+                        };
+
+                        InputManager.Current.ProcessInput(e);
+                    }
+                    
+                    // Note: Based on your requirements you may also need to fire events for:
+                    // RoutedEvent = Keyboard.PreviewKeyDownEvent
+                    // RoutedEvent = Keyboard.KeyUpEvent
+                    // RoutedEvent = Keyboard.PreviewKeyUpEvent
+                }
             }
         }
     }
